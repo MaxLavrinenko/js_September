3b4b6ca5690a35dc25203dd77b560b1eddaae0f4
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2021-11-19 15:18:39 +0200

    Update cw4.js

--- ls4/cw4.js
+++ ls4/cw4.js
@@ -68,8 +68,50 @@
 // srednie(arr);
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+// let arr = [1,2,-3,4,5,6,7,8,9,10];
+// function minMaxNum(arr){
+//     let min = arr[0];
+// 	let max = arr[0];
+//     for (const item of arr) {
+//         if(item < min){
+//             min = item;
+//         }
+// 		else if(item > max){
+// 			max = item
+// 		}
+//     } console.log(`maximal number ${max}`)
+//     return min;
+// }
+
+// console.log(minMaxNum(arr));
 
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+// arr = [];
+// function random (arr, size){
+// 	for (i=0;i<size;i++){
+// 		arr[i] = Math.round(Math.random()*100);
+// 	}
+// }
+// random(arr,5);
+// console.log(arr);
+
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+// arr = [];
+// function random (arr, size,limit=100){
+// 	for (i=0;i<size;i++){
+// 		arr[i] = Math.round(Math.random()*limit);
+// 	}
+// }
+// random(arr,20,50);
+// console.log(arr);
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+// let arr = [1,2,3]
+//     function reverse(arr) {
+//         let newArr = [];
+//         for (i = arr.length - 1, ni = 0; i > -1; i--, ni++) {
+//             newArr[ni] = arr[i];
+//         }
+//         return newArr;
+//     }
+// 	console.log(reverse(arr));
