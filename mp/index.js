6db88a636284fe17918b21985c02ccb9984da7b2
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-01-07 17:52:07 +0200

    mp

---
+++ mp/index.js
@@ -0,0 +1,32 @@
+// В index.html
+// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
+// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html,
+// которая имеет детальную информацию про объект на который кликнули.
+//index.html - все блоки с user - по 2 в ряд. кнопки/ссылки находяться под информацией про user.
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>{
+        return response.json();
+    })
+    .then(users=>{
+        let container = document.createElement('div');
+        container.classList.add('container')
+        document.body.appendChild(container);
+        for(let user of users){
+            let userDiv = document.createElement('div')
+            userDiv.classList.add('userdiv');
+            userDiv.innerHTML = `<h3> ID = ${user.id} </h3><h2> Name: ${user.name}</h2>`
+            container.appendChild(userDiv);
+            let userButton = document.createElement('button');
+            userButton.innerText = 'datails';
+            userDiv.appendChild(userButton);
+
+            userButton.onclick = function (){
+                localStorage.setItem('user',JSON.stringify(user))
+                window.location.href='user-details.html'
+                }
+
+        }
+
+})
---
+++ mp/user-details.js
@@ -0,0 +1,56 @@
+// На странице user-details.html:
+// 4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
+
+
+let user = JSON.parse(localStorage.getItem('user'));
+console.log(user);
+
+let userContainer = document.createElement('div');
+userContainer.classList.add('userContainer');
+let userDiv = document.createElement('div');
+userContainer.appendChild(userDiv);
+userDiv.innerHTML=`
+    <h1> User Name: ${user.name}</h1>
+    <h2> User Nick Name: ${user.username} Id User:${user.id}</h2>
+    <h3>Email : ${user.email}</br>
+    Phone: ${user.phone} </br>
+    Website: ${user.website}
+    </h3>
+    <p>adress: </br>
+    city: ${user.address.city}</br>
+    street: ${user.address.street}</br>
+    suite: ${user.address.suite}</br>
+    zipcode: ${user.address.zipcode}</br>
+    location: lat: ${user.address.geo.lat} , lng: ${user.address.geo.lng}
+    </br></br>
+    Information of company: </br> 
+    Company name: ${user.company.name} </br>
+    Company catch phrase: ${user.company.catchPhrase}</br>
+    business directions: ${user.company.bs}    
+     </p>`
+let postButton = document.createElement("button");
+postButton.innerText = 'post of current user';
+postButton.onclick = function (){
+    fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+        .then(response=> response.json())
+        .then(posts =>{
+            for (let post of posts) {
+                let titleDiv = document.createElement('div')
+                document.body.appendChild(titleDiv);
+                titleDiv.innerText = `Post title: ${post.title} `
+                let postButton = document.createElement("button");
+                postButton.innerText = 'post details';
+                titleDiv.appendChild(postButton);
+                postButton.onclick = function (){
+                    localStorage.setItem('post',JSON.stringify(post))
+                    window.location.href='post-details.html'
+                }
+
+            }
+        })
+    }
+
+document.body.append(userContainer,postButton);
